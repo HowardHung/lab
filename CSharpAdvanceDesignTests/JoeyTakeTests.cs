d9c35cf23a78295aea5d1bd8a1ea326b936418f5
Howard <xbgskg@yahoo.com.tw>
2020-02-28 16:33:25 +0800

    [refactor][take] extract parameter "count"

--- CSharpAdvanceDesignTests/JoeyTakeTests.cs
+++ CSharpAdvanceDesignTests/JoeyTakeTests.cs
@@ -1,34 +1,33 @@
-﻿using ExpectedObjects;
+﻿using System;
+using System.Collections.Generic;
+using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
-using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
-    [TestFixture()]
-    [Ignore("not yet")]
+    [TestFixture]
+    //[Ignore("not yet")]
     public class JoeyTakeTests
     {
-        [Test]
-        public void take_2_employees()
+        private IEnumerable<Employee> JoeyTake(IEnumerable<Employee> employees, int count)
         {
-            var employees = GetEmployees();
-
-            var actual = JoeyTake(employees);
-
-            var expected = new List<Employee>
+            var enumerator = employees.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
             {
-                new Employee {FirstName = "Joey", LastName = "Chen"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
-            };
+                var enumeratorCurrent = enumerator.Current;
+                if (index < count)
+                {
+                    yield return enumeratorCurrent;
+                }
+                else
+                {
+                    yield break;
+                }
 
-            expected.ToExpectedObject().ShouldEqual(actual);
-        }
-
-        private IEnumerable<Employee> JoeyTake(IEnumerable<Employee> employees)
-        {
-            throw new System.NotImplementedException();
+                index++;
+            }
         }
 
         private static IEnumerable<Employee> GetEmployees()
@@ -39,8 +38,40 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Tom", LastName = "Li"},
                 new Employee {FirstName = "David", LastName = "Chen"},
                 new Employee {FirstName = "Mike", LastName = "Chang"},
-                new Employee {FirstName = "Joseph", LastName = "Yao"},
+                new Employee {FirstName = "Joseph", LastName = "Yao"}
             };
+        }
+
+        [Test]
+        public void take_2_employees()
+        {
+            var employees = GetEmployees();
+
+            var actual = JoeyTake(employees, 2);
+
+            var expected = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"}
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+        [Test]
+        public void take_3_employees()
+        {
+            var employees = GetEmployees();
+
+            var actual = JoeyTake(employees, 3);
+
+            var expected = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"}
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
         }
     }
 }
