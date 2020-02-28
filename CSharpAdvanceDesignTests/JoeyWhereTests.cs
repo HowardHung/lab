be185c6cd784b82542338c2e8e3b89157e482f88
Howard <xbgskg@yahoo.com.tw>
2020-02-28 10:31:42 +0800

    [feat][where] complete JoeyWhereForEmployee()

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -9,11 +9,11 @@ namespace CSharpAdvanceDesignTests
     [TestFixture]
     public class JoeyWhereTests
     {
-        private List<Product> JoeyWhere(List<Product> products, Func<Product, bool> Predicate)
+        private List<Product> JoeyWhere(List<Product> products, Func<Product, bool> predicate)
         {
             var result = new List<Product>();
             foreach (var product in products)
-                if (Predicate(product))
+                if (predicate(product))
                     result.Add(product);
 
             return result;
@@ -70,6 +70,43 @@ namespace CSharpAdvanceDesignTests
             };
 
             expected.ToExpectedObject().ShouldMatch(actual);
+        }
+        [Test]
+        public void Find_the_first_name_length_less_than_5()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Claire", LastName = "Chen"},
+                new Employee {FirstName = "May", LastName = "Chen"},
+            };
+
+            var actual = JoeyWhereForEmployee(
+                employees, e => e.FirstName.Length < 5);
+
+            var expected = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "May", LastName = "Chen"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private List<Employee> JoeyWhereForEmployee(List<Employee> employees,
+            Func<Employee, bool> predicate)
+        {
+            var result = new List<Employee>();
+            foreach (var employee in employees)
+            {
+                if (predicate(employee))
+                {
+                    result.Add(employee);
+                }
+            }
+
+            return result;
         }
     }
 }
