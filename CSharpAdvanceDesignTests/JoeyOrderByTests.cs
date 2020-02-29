8a884b61d8ae31c7a04f48e6e994c54c04abe5a0
Howard <xbgskg@yahoo.com.tw>
2020-02-29 14:51:45 +0800

    dirty order thenby

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -19,19 +19,54 @@ namespace CSharpAdvanceDesignTests
                 var minElement = elements[0];
                 var index = 0;
                 for (var i = 1; i < elements.Count; i++)
+                {
                     if (stringComparer.Compare(elements[i].LastName, minElement.LastName) < 0)
                     {
                         minElement = elements[i];
                         index = i;
                     }
+                    else if (stringComparer.Compare(elements[i].LastName, minElement.LastName) == 0)
+                    {
+                        if (stringComparer.Compare(elements[i].FirstName, minElement.FirstName) < 0)
+                        {
+                            minElement = elements[i];
+                            index = i;
+                        }
+                    }
+                }
 
                 elements.RemoveAt(index);
                 yield return minElement;
             }
         }
 
+        //[Test]
+        //public void orderBy_lastName()
+        //{
+        //    var employees = new[]
+        //    {
+        //        new Employee {FirstName = "Joey", LastName = "Wang"},
+        //        new Employee {FirstName = "Tom", LastName = "Li"},
+        //        new Employee {FirstName = "Joseph", LastName = "Chen"},
+        //        new Employee {FirstName = "Joey", LastName = "Chen"}
+        //    };
+
+        //    var actual = JoeyOrderByLastName(employees);
+
+        //    var expected = new[]
+        //    {
+        //        new Employee {FirstName = "Joseph", LastName = "Chen"},
+        //        new Employee {FirstName = "Joey", LastName = "Chen"},
+        //        new Employee {FirstName = "Tom", LastName = "Li"},
+        //        new Employee {FirstName = "Joey", LastName = "Wang"}
+        //    };
+
+        //    expected.ToExpectedObject().ShouldMatch(actual);
+        //}
+
+
         [Test]
-        public void orderBy_lastName()
+        public void orderBy_lastName_then_firstName()
         {
             var employees = new[]
             {
@@ -45,8 +80,8 @@ namespace CSharpAdvanceDesignTests
 
             var expected = new[]
             {
-                new Employee {FirstName = "Joseph", LastName = "Chen"},
                 new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
                 new Employee {FirstName = "Tom", LastName = "Li"},
                 new Employee {FirstName = "Joey", LastName = "Wang"}
             };
